27d5520b0a5565f4627dc54403b9c2b38d245b19
apple <apple@Roberts-MacBook-Pro.local>
2018-08-28 17:54:36 -0500

    added variable declaration

--- main.swift
+++ main.swift
@@ -8,4 +8,16 @@
 
 import Foundation
 
-
+let sample1:UInt8 = 0x3A
+var sample2:UInt8 = 5
+var heartRate:Int = 85
+var deposits:Double = 135002796
+let acceleration:Float = 9.800
+var mass:Float = 14.6
+var distance:Double = 129.763001
+var lost:Bool = true
+var expensive:Bool = true
+var choice:Int = 2
+let integral:Character = "\u{222B}"
+let greeting:String = "Hello"
+var name:String = "Karen"
